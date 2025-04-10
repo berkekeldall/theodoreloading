b7597e40f7b7b91445e2301320dfec453f9378bf
berkekeldall <64058302+berkekeldall@users.noreply.github.com>
2025-04-10 16:52:46 +0300

    Update config.js

--- config.js
+++ config.js
@@ -18,29 +18,12 @@ config.background_images_duration = 5000
 config.background_images_fade_duration = 2000
 config.background_overlay = true
 config.background_darkening = 30
-config.music_enable = true
-config.music_show = true
+config.music_enable = false
+config.music_show = false
 config.music_autoplay = false
-config.music_playlist = [
-  {
-    name: "Jason Shaw - Sideways Samba - Audionautix.com",
-    file: "sideways_samba.ogg"
-  },
-  {
-    name: "Devil Survivor 2 - Exploration",
-    youtube: "https://www.youtube.com/watch?v=Zyu-BSdma3g"
-  },
-  {
-    name: "VA-11 Hall-A - Every Day Is Night",
-    youtube: "https://youtu.be/H-AfGh8gmiQ"
-  },
-  {
-    name: "Sonic Mania - Comfort Zone",
-    youtube: "f4CBQ9nnQSY"
-  }
-]
+config.music_playlist = []
 config.music_random_order = true
-config.music_volume = 15
+config.music_volume = 5
 config.music_gmod_volume = true
 config.messages_enable = true
 config.messages_list = [
